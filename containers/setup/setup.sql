136467bac12e4f56aac80004c14092be2a7b1ec3
Anish Thomas <anish.thomas@evolvussolutions.com>
2019-08-10 15:59:04 +0530

    completing the rest of the changes

--- containers/setup/setup.sql
+++ containers/setup/setup.sql
@@ -1,2 +1,4 @@
 DROP TABLE NAME_LIST;
 CREATE TABLE NAME_LIST ( id number, state nvarchar2(2), gender nvarchar2(1), year nvarchar2(4), name nvarchar2(255), usage number); 
+DROP TABLE STATE_MAP;
+CREATE TABLE STATE_MAP ( state_code nvarchar2(2), state_name nvarchar2(50)); 
